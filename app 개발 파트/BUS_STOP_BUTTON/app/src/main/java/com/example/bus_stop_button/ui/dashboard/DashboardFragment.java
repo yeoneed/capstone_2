8fc50d2fdff91aa30d9a5edb38fe707f980f60f2
yeojoo1224 <yeojoo1224@naver.com>
2020-10-19 15:40:01 +0900

    Add BUS_STOP_BUTTON

---
+++ app 개발 파트/BUS_STOP_BUTTON/app/src/main/java/com/example/bus_stop_button/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,35 @@
+package com.example.bus_stop_button.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.bus_stop_button.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_route, container, false);
+
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+            }
+        });
+        return root;
+    }
+}
